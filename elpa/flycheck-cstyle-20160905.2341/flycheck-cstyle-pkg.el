e6895c631815f28d83d62071280d345b239e46e9
wfw-pgr <wfw.pgr@gmail.com>
2022-04-30 09:08:10 +0900

    first commit

--- elpa/flycheck-cstyle-20160905.2341/flycheck-cstyle-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-cstyle" "20160905.2341" "Integrate cstyle with flycheck" '((flycheck "0.24") (emacs "24.4")) :commit "207285140a353d08cf1fc450cacab158bc98ba82" :authors '(("Alex Murray" . "murray.alex@gmail.com")) :maintainer '("Alex Murray" . "murray.alex@gmail.com") :url "https://github.com/alexmurray/flycheck-cstyle")
