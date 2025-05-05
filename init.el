09722fcd67b74e2c0c9e2ce9e7825832f5f5f513
wfw-pgr <wfw.pgr@gmail.com>
2025-05-05 16:08:17 +0900

    save at 20250505.. clean emacs.d from now

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (package-initialize)
 (init-loader-load)
+
 ;; (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,7 +14,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; )
+;; )
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- inits/62_ReSt.el
+++ inits/62_ReSt.el
@@ -13,3 +13,31 @@
 (setq frame-background-mode 'dark)
 ;; 全部スペースでインデントしましょう
 (add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
+
+
+;; --  文字色変更コマンド  -- ;;
+(defun rst-color-region (beg end color)
+  "Wrap the selected region with :COLOR:`...` for Sphinx."
+  (let ((start (format " :%s:`" color))
+        (end-marker "` "))
+    (save-excursion
+      (goto-char end)
+      (insert end-marker)
+      (goto-char beg)
+      (insert start))))
+
+;; --  mode 赤  -- ;;
+(defun rst-color-red-region (beg end)
+  "Wrap the selected region with :red:`...`"
+  (interactive "r")
+  (rst-color-region beg end "red"))
+
+;; --  mode 青  -- ;;
+(defun rst-color-blue-region (beg end)
+  "Wrap the selected region with :blue:`...`"
+  (interactive "r")
+  (rst-color-region beg end "blue"))
+
+;; 文字色変更コマンド ショートカット ;;
+(global-set-key (kbd "C-c r") #'rst-color-red-region)
+(global-set-key (kbd "C-c b") #'rst-color-blue-region)
